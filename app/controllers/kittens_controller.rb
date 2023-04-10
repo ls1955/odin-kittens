9a009b16227c4d9a1edab5b2c9e8a0f3b5c77abd
Cheong <xxlsxx1955@gmail.com>
2023-04-10 16:50:07 +0800

    Include kittens' routes
    Implement #edit, #update and #destroy method.

--- app/controllers/kittens_controller.rb
+++ app/controllers/kittens_controller.rb
@@ -1,11 +1,10 @@
 class KittensController < ApplicationController
+  before_action :set_kitten, only: %i[show edit update destroy]
   def index
     @kittens = Kitten.all
   end
 
-  def show
-    @kitten = Kitten.find params[:id]
-  end
+  def show; end
 
   def new
     @kitten = Kitten.new
@@ -17,11 +16,31 @@ class KittensController < ApplicationController
     if @kitten.save
       redirect_to @kitten
     else
-      render new, status: :unprocessable_entity
+      render :new, status: :unprocessable_entity
     end
   end
 
+  def edit; end
+
+  def update
+    if @kitten.update kitten_params
+      redirect_to @kitten, notice: "Information has been successfully updated."
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @kitten.destroy
+
+    redirect_to root_path, notice: "Kitten has been successfully removed."
+  end
+
   private
+
+  def set_kitten
+    @kitten = Kitten.find params[:id]
+  end
 
   def kitten_params
     params.require(:kitten).permit(:name, :age, :cuteness, :softness)
